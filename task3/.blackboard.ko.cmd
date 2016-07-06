bbd32a1622f07a38da5a4ea6f3c73433e1aa3b6c
mgozhev <mgozhev@gmail.com>
2016-07-06 04:05:12 +0300

    clean

--- task3/.blackboard.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/student/rtsoft/training/task3/blackboard.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.16.0-4-common/scripts/module-common.lds --build-id  -o /home/student/rtsoft/training/task3/blackboard.ko /home/student/rtsoft/training/task3/blackboard.o /home/student/rtsoft/training/task3/blackboard.mod.o
--- task3/.tmp_versions/blackboard.mod
+++
@@ -1,2 +0,0 @@
-/home/student/rtsoft/training/task3/blackboard.ko
-/home/student/rtsoft/training/task3/blackboard.o
